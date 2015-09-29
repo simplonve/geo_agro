541952de183e3c59534a55ebc873ffb55d2cd879
Xavier07 <albator98@hotmail.com>
2015-09-29 12:12:09 +0200

    Test connexion bdd

--- model/bdd_read_agro.php
+++ model/bdd_read_agro.php
@@ -2,5 +2,13 @@
 	// On récupère tout le contenu de la table geo-agro
 
       $read_data = $bdd->query('SELECT * FROM geo-agro');
+
+      while ($row = $read_data->fetch()){
+
+      echo $row['Nom'];
+
+      }
  
+ 	$read_data->closeCursor();
+
 ?>
